578b5f632c58ad2f211ef085db135112d1361187
Tejash1002 <tejash.burle@gmail.com>
2026-03-17 16:14:49 +0530

    HotelBookingApp

---
+++ YourHotel/src/HotelBookingApp.java
@@ -0,0 +1,7 @@
+public class HotelBookingApp {
+    public static void main(String args[]){
+        System.out.println("Welcome to Your Hotel");
+        System.out.println("Version: 1.0");
+        System.out.println("Application Started Successfully");
+    }
+}
